d6b05df3cce431a429d56e6a8dcfad20ebd9f765
Faisal Ahmed <fahmed@insightintechnology.com>
2025-03-18 03:41:48 +0600

    Product Page

--- assets/js/app.js
+++ assets/js/app.js
@@ -382,10 +382,10 @@ if($('[data-brand-logo-slide]').length > 0){
 $(window).on('scroll', function () {
     if ($(window).scrollTop() > 100)
     {
-      $(".et-sticky-addto-cart-section").addClass("active");
+      $("[data-sticky-cart]").addClass("active");
        
     } else{
-        $(".et-sticky-addto-cart-section").removeClass("active");
+        $("[data-sticky-cart]").removeClass("active");
     }
 });
 var buttonPlus  = $(".et-custom-qty__btn-plus");
@@ -402,4 +402,60 @@ buttonMinus.on("click", function(){
     }
 });
 
-console.log("Faisal");
+$('[data-ajax-cart]').on("click", function(event){
+    event.preventDefault();
+    $(this).addClass("active");
+    var form = $(this).parent().parent();
+    var url = form.attr('action');
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: form.serialize(),
+        success: function(data) {
+            fetch('/api/storefront/cart', {
+                credentials: 'include'
+            }).then(function(response) {
+                return response.json();
+            }).then(function(data) {
+                var num = 0;
+                for (let i = 0; i < data[0].lineItems.physicalItems.length; i++) {
+                    num += data[0].lineItems.physicalItems[i].quantity;
+                };
+                $('.et-ajax-cart-button').removeClass("active");
+                $('.cart-quantity').addClass("countPill--positive");
+                $(".cart-quantity").html(num);
+                //console.log(data);
+            });
+        },
+        error: function(data) {}
+    });
+});
+
+$('[data-ajax-cart]').on("click", function(event){
+    event.preventDefault();
+    $(this).addClass("active");
+    var form = $(this).parent().parent();
+    var url = form.attr('action');
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: form.serialize(),
+        success: function(data) {
+            fetch('/api/storefront/cart', {
+                credentials: 'include'
+            }).then(function(response) {
+                return response.json();
+            }).then(function(data) {
+                var num = 0;
+                for (let i = 0; i < data[0].lineItems.physicalItems.length; i++) {
+                    num += data[0].lineItems.physicalItems[i].quantity;
+                };
+                $('.et-ajax-cart-button').removeClass("active");
+                $('.cart-quantity').addClass("countPill--positive");
+                $(".cart-quantity").html(num);
+                //console.log(data);
+            });
+        },
+        error: function(data) {}
+    });
+});
